1266dcca8b51cb42c7b86d63f76496d68199fea1
Alejo Mendoza <alejandro@skyhitzmusic.com>
2021-08-24 14:04:05 -0500

    add api

--- src/components/elements/VoteButton.tsx
+++ src/components/elements/VoteButton.tsx
@@ -5,6 +5,7 @@ import useAuth from 'src/stores/useAuth';
 import useBallot from 'src/stores/useBallot';
 
 import Button from 'src/components/elements/Button';
+import { approveProject, unapproveProject } from 'src/stores/api';
 
 const VoteButton = ({ name, slug }: { name: string; slug: string }) => {
   const discordToken = useAuth((state) => state.discordToken);
@@ -13,6 +14,22 @@ const VoteButton = ({ name, slug }: { name: string; slug: string }) => {
   if (!discordToken) return null;
 
   const isSelected = includes(slug);
+
+  const handleAddApprovedProject = async() => {
+    addApprovedProject(slug, name);
+    const res = await approveProject(slug);
+    if (!res.id){
+      removeProject(slug)
+    }
+  }
+
+  const handleRemoveProject = async() => {
+    removeProject(slug);
+    const res = await unapproveProject(slug);
+    if (!res.id){
+      removeProject(slug)
+    }
+  }
 
   return (
     <Button
