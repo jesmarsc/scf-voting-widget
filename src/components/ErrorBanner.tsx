ff8bfb9ffa1fc54a059dd29621bcf515553ca5c9
Alejo Mendoza <alejandro@skyhitzmusic.com>
2021-08-26 14:14:48 -0500

    clear error

--- src/components/ErrorBanner.tsx
+++ src/components/ErrorBanner.tsx
@@ -5,10 +5,14 @@ import define from 'preact-custom-element';
 import Container from 'src/components/elements/Container';
 
 const ErrorBanner = () => {
-  const { error } = useAuth();
+  const { error, clearAuth } = useAuth();
 
   if (!error) return null;
-  return <Container danger>{error}</Container>;
+  return (
+    <Container danger onClick={() => clearAuth()}>
+      {error}
+    </Container>
+  );
 };
 
 define(ErrorBanner, 'error-banner');
