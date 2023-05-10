6d901ae9616db6b6bd2ccd7f4865822bf4ef3b49
Alejo Mendoza <alejandro@skyhitzmusic.com>
2023-05-10 13:42:10 -0500

    add gap

--- src/components/VerifyBadges/index.tsx
+++ src/components/VerifyBadges/index.tsx
@@ -11,7 +11,7 @@ import { getProofTxt, updatePublicKeys } from 'src/utils/api';
 import { downloadTxt } from 'src/utils';
 
 const Container = styled('div')([
-  tw`block relative py-2 px-4 font-bold font-sans rounded border-none tracking-wide transition-colors text-black shadow-purple mx-auto max-w-2xl`,
+  tw`flex flex-col gap-2 py-2 px-4 font-bold font-sans rounded border-none tracking-wide transition-colors text-black shadow-purple mx-auto max-w-2xl`,
   ({ danger }: { danger?: boolean }) =>
     danger ? tw`bg-stellar-salmon shadow-salmon` : '',
 ]);
